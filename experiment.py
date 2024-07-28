ae4c3735b7ee514f41bdf797f46042341f0c1378
Ameya Sanjanita <124460697+ameya-sanz@users.noreply.github.com>
2024-07-28 14:42:21 -0400

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -16,6 +16,7 @@ indices = df['Index'].tolist()
 refractive_indices = dict(zip(mediums, indices))
 
 app = dash.Dash(__name__)
+server = app.server
 
 app.layout = html.Div([
     html.H1("Snell's Law Visualization"),
